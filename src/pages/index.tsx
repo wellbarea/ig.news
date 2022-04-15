e75f501a8cf9b1bf1e517d3d4a54a3528209c33a
Wellington Barea <wellington.gabriel90@gmail.com>
2022-04-15 18:09:32 -0300

    request api strape

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
+import { GetServerSideProps } from 'next';
+
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import Image from 'next/image';
 import Avatar from '../../public/images/avatar.svg';
-
 import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,14 +28,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world. </h1>
           <p>
             Get access to all publications <br/>
-            <span>for $9.90 moth</span>
+            <span>for {product.amount} moth</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image src={Avatar} alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KoqzVA4hBHFjOFEl9V97vX5');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
