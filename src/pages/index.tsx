ca8c24fc115104404009d1a81b099392ceb8e52c
Wellington Barea <wellington.gabriel90@gmail.com>
2022-04-15 10:16:37 -0300

    feat: add new component => Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from 'next/head';
 
+import Image from 'next/image';
+import Avatar from '../../public/images/avatar.svg';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title> Inicio | ig.news </title>
+        <title> Home | ig.news </title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, welcome </span>
+          <h1>News about the <span>React</span> world. </h1>
+          <p>
+            Get access to all publications <br/>
+            <span>for $9.90 moth</span>
+          </p>
+        </section>
+
+        <Image src={Avatar} alt="Girl coding" />
+      </main>
     </>
   )
 }
