f3f3bd8f3a3818efbc47c6f87ff8b51ef5230be9
Wellington Barea <wellington.gabriel90@gmail.com>
2022-04-15 11:16:19 -0300

    feat: add component =>  SigInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
+import { SingInButton } from '../SingInButton';
+
+import Image from 'next/image';
+import Logo from '../../../public/images/logo.svg';
+
 import styles from './styles.module.scss';
 
 export function Header () {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img 
-                    src="/images/logo.svg" 
+                <Image 
+                    src={Logo} 
                     alt="ig-news" />
                 <nav>
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
                 </nav>
+
+                <SingInButton />
             </div>
         </header>
     )
