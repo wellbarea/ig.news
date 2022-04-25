b358ac77f512bf466ce204103c3a0b5f02f37c82
Wellington Barea <wellington.gabriel90@gmail.com>
2022-04-25 20:14:25 -0300

    feat: add connection next-auth

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,20 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SingInButton() {
 
-    const isUserLoggedIn = true;
+    const [ session ] = useSession();
 
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+    return session ? (
+        <button 
+            type="button"
+            className={styles.signInButton} 
+            onClick={() => signOut()}>
             <FaGithub color='#04d361' />
-            Diego Fernandes
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button 
+            type="button"
+            className={styles.signInButton} 
+            onClick={() => signIn('github')}
+            >
             <FaGithub color='#eba417' />
             Sign in with Github
         </button>
