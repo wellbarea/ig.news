98ec6768eb1ce6ff17a45bcb4eb23a57706a57cd
Wellington Barea <wellington.gabriel90@gmail.com>
2022-06-03 16:49:07 -0300

    feat: add subscribe button

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
-import { query as queryFauna } from 'faunadb';
+import { query as q } from 'faunadb'
+
+import NextAuth from 'next-auth'
+
+import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fauna';
 
@@ -13,40 +15,74 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
+
       try {
         await fauna.query(
-          queryFauna.If(
-            queryFauna.Not(
-              queryFauna.Exists(
-                queryFauna.Match(
-                  queryFauna.Index('user_by_email'),
-                  queryFauna.Casefold(user.email)
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
                 )
               )
             ),
-            queryFauna.Create(
-              queryFauna.Collections('users'),
-              {
-                data: {
-                  email
-                } 
-              }
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
             ),
-            queryFauna.Get(
-              queryFauna.Match(
-                queryFauna.Index('user_by_email'),
-                queryFauna.Casefold(user.email)
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
               )
             )
           )
         )
-
-        return true;
+        return true
       } catch {
-        return false;
+        return false
       }
+
     }
   }
 })
