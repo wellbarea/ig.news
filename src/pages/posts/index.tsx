432c0a4195d515194d747884e2b316519b020d8b
Wellington Barea <wellington.gabriel90@gmail.com>
2022-06-07 07:36:30 -0300

    feat: formated data

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,21 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic'
+import { RichText } from 'prismic-dom'
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,21 +25,13 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong> packages and web page editors now use Lorem Ipsum as their </strong>
-                        <p>default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years</p>
-                    </a>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong> packages and web page editors now use Lorem Ipsum as their </strong>
-                        <p>default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years</p>
-                    </a>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong> packages and web page editors now use Lorem Ipsum as their </strong>
-                        <p>default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years</p>
-                    </a>
+                    {posts.map(post => (
+                        <a href='#' key={post.slug}>
+                            <time>{post.updatedAt}</time>
+                            <strong> {post.title} </strong>
+                            <p> {post.excerpt} </p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
@@ -47,11 +51,22 @@ export const getStaticProps: GetStaticProps = async () => {
         }
     )
 
-    console.log('response', response);
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
 
     return {
         props: {
-
+            posts
         }
     }
 }
